d73a745d870aeed20983a12a7f664ca18f776e4b
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-10-08 20:14:41 -0500

    added testGetScore

--- app/src/test/java/com/google/engedu/blackhole/BlackHoleBoardTest.java
+++ app/src/test/java/com/google/engedu/blackhole/BlackHoleBoardTest.java
@@ -26,6 +26,30 @@ import java.util.Stack;
 
 import static org.junit.Assert.*;
 
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BlackHoleBoardTest {
 
     private BlackHoleBoard b;
@@ -99,8 +123,6 @@ public class BlackHoleBoardTest {
 
         assertEquals(newBoard.getScore(),-41);
         //hole is at 3,3
-
-
 
 
 
@@ -196,7 +218,7 @@ public class BlackHoleBoardTest {
         //TODO
     }
 
-    
+
 
     @Test
     public void testGetMovesFromMinMax() {
